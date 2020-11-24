451288fa498066bc7b3516690abd76509f65984d
skinny <globchansky.denis@gmail.com>
2020-11-24 12:33:33 +0300

    Добавил в письме для подтверждения мыла токен в открытом виде

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -384,8 +384,10 @@ Content-type: text/html; charset=utf8
 	<input type="hidden" name="x-reg-token" value="`+xRegToken+`">
 	<input type="submit" value="Click to confirm mail">
 </form>
-<a href="http://localhost:3000/user/update/status/?x-reg-token=`+xRegToken+`">click to confirm mail</a>
-if this letter came by mistake - delete it</span>
+<a href="http://localhost:3000/user/update/status/?x-reg-token=`+xRegToken+`">click to confirm mail</a> 
+ `+xRegToken+` 
+if this letter came by mistake - delete it 
+</span>
 `
 
 	if err := smtp.SendMail(mailConf.Host+":587",
